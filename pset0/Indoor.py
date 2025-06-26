35d8856756f78b6e6644b5acbdf6c80ebbe27289
Luis Enrique <fernandomorenoluis123@gmail.com>
2025-06-25 20:30:36 -0600

    Solution to pset0 indoor.py

---
+++ pset0/Indoor.py
@@ -0,0 +1,9 @@
+def main():
+
+    #Prompt the user to enter the phrase
+    phrase = str(input('\n')).lower() #Apply the use of the lower function to convert any uppercase string to lowercase.
+    
+    print(phrase)
+    
+main()
+    
